63e5306665168dd23684aada111f985bda8359a9
sammuel-kouassi <samuuel.kouassi2026@gmail.com>
2026-03-15 23:55:41 +0000

    CORS ouvert

--- src/main/java/com/example/kingtechbackend/controller/AuthController.java
+++ src/main/java/com/example/kingtechbackend/controller/AuthController.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:4200") // TODO: Ajouter l'URL de Vercel plus tard
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
